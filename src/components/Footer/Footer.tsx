23f1eabe7bf836f3bb44abc90c84e136ff00f154
Пользователь <cadsergrf@yandex.ru>
2024-07-03 01:16:23 +0700

    feat: верстка footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,29 @@
-// import styles from './Footer.module.css'
+import { Link } from 'react-router-dom';
+
+import styles from './Footer.module.css'
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className={styles.wrapper}>
+      <Link to="/" className={styles.logo}>
+        Goods4you
+      </Link>
+      <nav>
+        <ul className={styles.menu}>
+          <li>
+            <Link to="/catalog" className={styles.menu__item}>
+              Catalog
+            </Link>
+          </li>
+          <li>
+            <Link to="/faq" className={styles.menu__item}>
+              FAQ
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </footer>
+  );
 };
 
 export { Footer };
